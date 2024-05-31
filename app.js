7ddbf3d3e9278cbd952204dd1850890fc1407dc8
Giovanny Salcedo <giovanny.salcedo@genstudents.org>
2024-05-30 22:51:03 -0500

    feature done

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-console.log("Hola 👌");
+let valuesInput = document.getElementById("valuesInput");
 
-let buttonInput = document.getElementById("buttonInput")
+let printFahrenheit = document.getElementById("resultsFahrenheit");
+let printKelvin = document.getElementById("resultsKelvin");
 
-buttonInput.addEventListener("click", () => {
-  console.log("Hola");
-})
+let alert = document.getElementById("alert")
+
+valuesInput.addEventListener('input', () => {
+  let values = valuesInput.value;
+  convertTemperature(values);
+});
+
+function convertTemperature(temperature) {
+  if (!isNaN(temperature) && temperature.trim() !== "") {
+    let temperatureFahrenheit = (parseFloat(temperature) * 9 / 5) + 32;
+    let temperatureKelvin = parseFloat(temperature) + 273.15;
+
+    printFahrenheit.textContent = `Temperatura en grados Fahrenheit: ${temperatureFahrenheit.toFixed(2)}`;
+    printKelvin.textContent = `Temperatura en Kelvin: ${temperatureKelvin.toFixed(2)}`;
+    alert.classList.add("d-none");
+  } else {
+    alert.classList.remove("d-none");
+    valuesInput.value = "";
+  }
+}
